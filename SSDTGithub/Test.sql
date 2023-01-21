c7d5547b957d8a0d30dd8a43e82e75789eb6c26f
maziar2016 <maziar.namdar@sickkidsfoundation.com>
2023-01-21 11:52:13 -0500

    Test for local repo

--- SSDTGithub/Test.sql
+++ SSDTGithub/Test.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [FirstName] VARCHAR(50) NULL, 
     [LastName] VARCHAR(50) NULL, 
-    [DateAdded] DATETIME NULL DEFAULT getdate()
+    [DateAdded] DATETIME NULL DEFAULT getdate(), 
+    [DateModified] DATETIME NULL DEFAULT getdate()
 )

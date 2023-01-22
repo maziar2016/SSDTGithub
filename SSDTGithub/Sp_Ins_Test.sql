6ae162f7abf0a74e6f10e69395efd4e3edd1fd89
maziar2016 <maziar.namdar@sickkidsfoundation.com>
2023-01-22 10:32:35 -0500

    BranchTesting

---
+++ SSDTGithub/Sp_Ins_Test.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[Sp_Ins_Test]
+	@FirstName NVARCHAR(100),
+	@LastName NVARCHAR(100),
+	@TelNo NVARCHAR(50)
+AS
+	INSERT INTO test (FirstName,lastname,telno)
+	VALUES(@FirstName,@LastName,@TelNo)
+RETURN 0
